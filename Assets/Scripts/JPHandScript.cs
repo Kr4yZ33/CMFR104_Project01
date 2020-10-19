7291e3898c7e7447f78103ae0e1f5df26f0ad136
jpernesz <1020894@student.sae.edu.au>
2020-10-19 19:47:44 +1100

    Added content from CMR103 which is also relevant to this project and crosses over

---
+++ Assets/Scripts/JPHandScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JPHandScript : MonoBehaviour
+{
+    public JPColourChange theCube;
+    public TrackController trackController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var hand = GetComponent<OVRHand>();
+        bool isIndexFingerPinching = hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+
+        if (isIndexFingerPinching)
+        {
+            //is pinching., do something
+            theCube.SetGreen();
+            trackController.isHeld = false;
+            Debug.Log("FingerPinch");
+        }
+        else
+        {
+            //not pinching
+            theCube.SetRed();
+        }
+
+        float ringFingerPinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Ring);
+    }
+}
