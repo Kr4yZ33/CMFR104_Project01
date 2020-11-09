7b7a280f0462fb9eb302ce3a7819b00dbfdd5e0b
jpernesz <1020894@student.sae.edu.au>
2020-11-10 10:55:15 +1100

    Added a second scene for track building

---
+++ Assets/Scripts/VRRig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRig : MonoBehaviour
+{
+    public Transform touchL;
+    public Transform touchR;
+
+    // Update is called once per frame
+    void Update()
+    {
+        touchL.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        touchR.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+
+        touchL.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        touchR.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+    }
+}
