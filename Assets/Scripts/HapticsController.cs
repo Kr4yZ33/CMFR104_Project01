e41272df4e61ed85de118a456172152d6a1bf5d8
jpernesz <1020894@student.sae.edu.au>
2020-11-19 06:53:52 +1100

    Audio haptics now working

--- Assets/Scripts/HapticsController.cs
+++ Assets/Scripts/HapticsController.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class HapticsController : MonoBehaviour
 {
+    public TrackSnapConnection trackSnapConnection;
+
     OVRHapticsClip buzz;
     public AudioClip trackConnect;
-    
+    public bool trackConnected;
+
     // Start is called before the first frame update
     void Start()
     {
         buzz = new OVRHapticsClip(trackConnect);
+    }
+
+    private void Update()
+    {
+        if(trackConnected == true)
+        {
+            OVRHaptics.LeftChannel.Mix(buzz);
+            OVRHaptics.RightChannel.Mix(buzz);
+        }
     }
 
     private void OnTriggerEnter(Collider hand)
--- Assets/Scripts/TrackSnapConnection.cs
+++ Assets/Scripts/TrackSnapConnection.cs
@@ -4,47 +4,27 @@ using UnityEngine;
 
 public class TrackSnapConnection : MonoBehaviour
 {
+    public HapticsController hapticsController;
+    
     public AudioSource audioSource;
     public AudioClip trackConnectedClip;
     
     public Transform closestEdge;
-    //public bool connectionClipPlayed;
     public float volume = 0.5f;
-
-    private OVRGrabbable ovrGrabbable;
-
-    private void Start()
-    {
-        ovrGrabbable = GetComponent<OVRGrabbable>();
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        //if(connectionClipPlayed == true)
-        //{
-            //return;
-        //}
         
         if(other.gameObject.CompareTag("TrackEdge"))
         {
             closestEdge = other.gameObject.transform;
             audioSource.PlayOneShot(trackConnectedClip, volume);
-            PlayVibrationHaptics();
+            hapticsController.trackConnected = true;
         }
     }
 
-    //private void OnTriggerExit(Collider other)
-    //{
-        //if (other.gameObject.CompareTag("TrackEdge"))
-        //{
-            //connectionClipPlayed = false;
-        //}
-    //}
-
-    void PlayVibrationHaptics()
+    private void OnTriggerExit(Collider other)
     {
-        VibrationManager.singleton.TriggerVibration(trackConnectedClip, ovrGrabbable.grabbedBy.GetController());
-        
-        //connectionClipPlayed = true;
+        hapticsController.trackConnected = false;
     }
 }
