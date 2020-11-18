643d8cf465d5c94abd71ec66c6cffd83e8d15030
jpernesz <1020894@student.sae.edu.au>
2020-11-18 22:01:56 +1100

    Raycast from finger now working on R trigger press

--- Assets/Scripts/TrackSnapConnection.cs
+++ Assets/Scripts/TrackSnapConnection.cs
@@ -5,12 +5,39 @@ using UnityEngine;
 public class TrackSnapConnection : MonoBehaviour
 {
     public Transform closestEdge;
+    public bool connectionClipPlayed;
+    public AudioClip trackConnectClip;
+    
+    private void Start()
+    {
+        //ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("TrackEdge"))
         {
             closestEdge = other.gameObject.transform;
+            
+            if(connectionClipPlayed == true)
+            {
+                return;
+            }
+
+            //if()
+
+            //VibrationManager.singleton.TriggerVibration(trackConnectClip, ovrGrabbable.grabbedBy.GetController());
+            GetComponent<AudioSource>().PlayOneShot(trackConnectClip);
+            connectionClipPlayed = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("TrackEdge"))
+        {
+            connectionClipPlayed = false;
         }
     }
 }
