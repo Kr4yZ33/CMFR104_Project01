887a0e5527b995818294490665864f7128c692a5
jpernesz <1020894@student.sae.edu.au>
2020-11-30 14:53:53 +1100

    Got tablet setup again after accidentially removing it from my scene

--- Assets/Scripts/RigScaleController.cs
+++ Assets/Scripts/RigScaleController.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 public class RigScaleController : MonoBehaviour
 {
     public GameObject smallScaleRig;
+    public GameObject smallScaleRigBelt;
     public GameObject largeScaleRig;
+    public GameObject largeScaleRigBelt;
     public BoxCollider buildPlatformCollider;
     
 
     public void ChangeScaleToSmall()
     {
         largeScaleRig.SetActive(false);
+        largeScaleRigBelt.SetActive(false);
         //gameObject.transform.localScale = new Vector3(1, 1, 1);
         buildPlatformCollider.enabled = true;
         smallScaleRig.SetActive(true);
+        smallScaleRigBelt.SetActive(true);
         //gameObject.transform.position = smallRigSpawnTransform.transform.position;
     }
 
     public void ChangeScaleToLarge()
     {
         smallScaleRig.SetActive(false);
+        smallScaleRigBelt.SetActive(false);
         //gameObject.transform.localScale = new Vector3(10, 10, 10);
         //gameObject.transform.position = largeRigSpawnTransform.position;
         //gameObject.transform.rotation = largeRigSpawnTransform.rotation;
         buildPlatformCollider.enabled = false;
         largeScaleRig.SetActive(true);
+        largeScaleRigBelt.SetActive(true);
     }
 
     //public void IncreaseSCalebyOne()
