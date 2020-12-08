3ea9e95b5edf890353494b0eb57535cb12412b0c
jpernesz <1020894@student.sae.edu.au>
2020-12-08 11:11:51 +1100

    Added start menu UI Canvas

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+   // Update is called once per frame
+    void Update()
+    {
+        var currentPos = transform.position;
+        transform.position = Vector3(Mathf.Round(currentPos.x),
+                                     Mathf.Round(currentPos.y),
+                                     Mathf.Round(currentPos.z));
+    }
+
+    private Vector3 Vector3(float v1, float v2, float v3)
+    {
+        throw new NotImplementedException();
+    }
+}
