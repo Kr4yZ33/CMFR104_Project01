40d561235cd99ae56b167643c02873769c7188ba
jpernesz <1020894@student.sae.edu.au>
2020-12-13 23:30:54 +1100

    Improving UI elements, added light colours for UI slections

--- Assets/Scripts/HoverChange.cs
+++ Assets/Scripts/HoverChange.cs
@@ -4,20 +4,8 @@ using UnityEngine;
 
 public class HoverChange : MonoBehaviour
 {
-    //Renderer r;
-    //Color originalColor;
+    
     public GameObject trackMesh;
-
-    //void Start()
-    //{
-        //r = GetComponent<Renderer>();
-        //originalColor = r.material.color;
-    //}
-
-    //public void MakeGrey()
-    //{
-        //r.material.color = Color.grey;
-    //}
 
     public void HoverColour()
     {
@@ -26,7 +14,6 @@ public class HoverChange : MonoBehaviour
 
     public void MakeOriginalColour()
     {
-        //r.material.color = originalColor;
         trackMesh.SetActive(true);
     }
 }
---
+++ Assets/Scripts/HoverChangeUIButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverChangeUIButtons : MonoBehaviour
+{
+    Renderer r;
+
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    public void MakeGreen()
+    {
+        r.material.color = Color.green;
+    }
+
+    public void MakeRed()
+    {
+        r.material.color = Color.red;
+    }
+}
